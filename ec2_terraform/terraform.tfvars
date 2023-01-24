5a4c2572b4399c9c3c050918428f061e1636101b
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2023-01-24 08:29:15 +0300

    Terraform v1:0

---
+++ ec2_terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIA5N62VWDILRZ5RQ52"
+secret_key = "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"
---
+++ s3_terraform/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+
+    access_key = "${var.aws_access_key}"
+
+    secret_key = "${var.aws_secret_key}"
+
+    region = "${var.region}"
+
+}
+
+resource "aws_s3_bucket" "temps3" {
+
+    bucket = "${var.bucket_name}" 
+
+    acl = "${var.acl_value}"   
+
+}
+
